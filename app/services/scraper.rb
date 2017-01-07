7ae5db414a19edc3ed51aa500561e43d903f1415
Nayaabkhan Khan <khannayaab@gmail.com>
2017-01-07 12:05:19 +0530

    Refactor fetchers

--- app/services/scraper.rb
+++ app/services/scraper.rb
@@ -6,9 +6,28 @@ class Scraper
   def self.fetch(product_page)
     case product_page.site.name
     when 'Amazon'
-      from_amazon(product_page)
+      result = from_amazon(product_page)
     when 'Snapdeal'
-      from_snapdeal(product_page)
+      result = from_snapdeal(product_page)
+    end
+
+    if result > 0
+      latest = product_page.latest_price
+      if !latest || latest.price != result
+        PriceHistory.create({ :product_page => product_page, :price => result })
+
+        product_page.in_stock = true
+        product_page.save
+
+        if latest
+          AlertMailer.price_change_email(product_page, latest.price, result).deliver_now
+        end
+      end
+    elsif result == 0
+      product_page.in_stock = false
+      product_page.save
+    else
+      raise 'Warning: block not found or block content bad'
     end
   end
 
@@ -23,58 +42,29 @@ class Scraper
 
   def self.from_amazon(product_page)
     document = Nokogiri::HTML(self.safe_open(product_page.url))
-
-    latest = product_page.latest_price
-
     price_block = document.at_css('#olp_feature_div .a-color-price')
 
-    # did we find the block?
     if price_block && price_block.text
-      price = price_block.text.strip.delete('  ,').to_i
-      if !latest || latest.price != price
-        PriceHistory.create({ :product_page => product_page, :price => price })
-
-        product_page.in_stock = true
-        product_page.save
-
-        if latest
-          AlertMailer.price_change_email(product_page, latest.price, price).deliver_now
-        end
-      end
+      return price_block.text.strip.delete('  ,').to_i
     elsif document.at_css('#outOfStock')
-      # mark as out of stock
-      product_page.in_stock = false
-      product_page.save
+      return 0
     else
-      # send a warning mail with product and page details
-      puts 'warning: block not found or block content bad'
+      return -1
     end
   end
 
   def self.from_snapdeal(product_page)
     document = Nokogiri::HTML(self.safe_open(product_page.url))
 
-    latest = product_page.latest_price
     price_tag = document.at_css('input#productPrice')
     sold_out_tag = document.at_css('input#soldOut')
 
     if sold_out_tag && sold_out_tag[:value] == 'true'
-      product_page.in_stock = false
-      product_page.save
+      return 0
     elsif price_tag
-      price = price_tag[:value].to_i
-      if !latest || latest.price != price
-        PriceHistory.create({ :product_page => product_page, :price => price })
-
-        product_page.in_stock = true
-        product_page.save
-
-        if latest
-          AlertMailer.price_change_email(product_page, latest.price, price).deliver_now
-        end
-      end
+      return price_tag[:value].to_i
     else
-      puts 'warning: block not found or block content bad'
+      return -1
     end
   end
 end
